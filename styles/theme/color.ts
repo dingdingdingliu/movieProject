bb628b52e4df6a3088ea58fed6ae16a4b2337674
Kristy Liu <kristy.liu@plantsist.com>
2022-11-08 22:12:58 +0800

    feat: use Next.js 12 & theme setting

---
+++ styles/theme/color.ts
@@ -0,0 +1,13 @@
+interface Color {
+	[propName: string]: any
+}
+
+let colorTheme: Color = {
+	colorWhite: '#FFFFFF',
+	colorBlack: '#161616',
+	colorGrey: '#686B72',
+	colorPink: '#C10171',
+	colorPurple: '#5C00F2'
+}
+
+export default colorTheme
